7612cd0412dfbe190bff86fd1d1aa07fdcb03624
Rafael Jaques <rafael@phpit.com.br>
2016-07-14 22:15:27 -0300

    Exemplo browserwindow

---
+++ 4-api/ex-browserwindow.js
@@ -0,0 +1,12 @@
+// No processo principal
+const {BrowserWindow} = require('electron');
+
+// Ou no processo renderer
+const {BrowserWindow} = require('electron').remote;
+
+let win = new BrowserWindow({width: 800, height: 600});
+win.on('closed', () => {
+  win = null;
+});
+
+win.loadURL('http://rafajaques.com.br');
